269b8dfd9e3fac4ab05e44c2fcb04970e695b88c
super <qanhdk@gmail.com>
2017-03-10 15:38:05 +0700

    change redis db

--- config/database.php
+++ config/database.php
@@ -122,7 +122,7 @@ return [
             'host' => env('REDIS_HOST', 'localhost'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => 3,
+            'database' => 4,
         ],
         'session' => [
             'host' => env('REDIS_HOST', 'localhost'),
