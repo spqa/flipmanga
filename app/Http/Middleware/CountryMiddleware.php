c47b1f992a61a6d84f88e27d76a85f20d71997a9
super <qanhdk@gmail.com>
2017-03-11 10:59:34 +0700

    truyentranh18

--- app/Http/Middleware/CountryMiddleware.php
+++ app/Http/Middleware/CountryMiddleware.php
@@ -9,18 +9,22 @@ class CountryMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        if (strcmp(config('app.env'),'production')==0){
-            if (strcmp($_SERVER["HTTP_CF_IPCOUNTRY"],'VN')==0){
-                return $next;
-            }else{
-                abort(522);
+        if (strcmp(config('app.env'), 'local') == 0) {
+            if (isset($_SERVER["HTTP_CF_IPCOUNTRY"])) {
+                if (strcmp($_SERVER["HTTP_CF_IPCOUNTRY"], 'VN') == 0) {
+                    return $next($request);
+                } else {
+                    abort(522);
+                }
             }
+
         }
+        return $next($request);
     }
 }
