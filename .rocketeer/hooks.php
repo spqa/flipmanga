46b8c40529270082c559ad887f44fd809a53cd81
super <qanhdk@gmail.com>
2017-03-14 10:05:28 +0700

    auto comicvn

--- .rocketeer/hooks.php
+++ .rocketeer/hooks.php
@@ -30,7 +30,7 @@ return [
             'php artisan config:cache'
         ],
         'cleanup' => [
-            'ln -s storage/app/public/images public/'
+            'ln -s /home/www/truyentranh18/truyentranh18/shared/storage/app/public/images public/'
         ],
     ],
 
