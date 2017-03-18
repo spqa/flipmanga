9adda6bfe31d5ad7a8e274f71eedd8fd0edf62a5
super <qanhdk@gmail.com>
2017-03-18 07:55:36 +0700

    box chat and upload manga

--- app/Http/Controllers/MangaController.php
+++ app/Http/Controllers/MangaController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Manga;
+use App\UserUpload;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Request;
 
 class MangaController extends Controller
 {
@@ -20,10 +22,10 @@ class MangaController extends Controller
 //            dd($manga->getCacheKey('favorite'));
             $suggestion = Manga::inRandomOrder()->take(6)->get();
             $main_genre = $manga->getCachedGenres()->sortBy('name')->first();
-            if (auth()->check()){
-                $is_fav=Redis::sismember($manga->getCacheKey('favorite'),auth()->user()->id);
+            if (auth()->check()) {
+                $is_fav = Redis::sismember($manga->getCacheKey('favorite'), auth()->user()->id);
             }
-            return view('manga.manga_show', compact('manga', 'suggestion', 'main_genre','is_fav'));
+            return view('manga.manga_show', compact('manga', 'suggestion', 'main_genre', 'is_fav'));
 
         }
 
@@ -33,9 +35,9 @@ class MangaController extends Controller
         $next_link = $manga->chapters()->whereChapterNumber(DB::raw('(SELECT MIN(chapter_number) FROM chapters WHERE chapter_number > ' . $chapter->chapter_number . ' AND manga_id =' . $manga->id . ')'))->first();
         $prev_link = $manga->chapters()->whereChapterNumber(DB::raw('(SELECT MAX(chapter_number) FROM chapters WHERE chapter_number < ' . $chapter->chapter_number . ' AND manga_id =' . $manga->id . ')'))->first();
         $array_img = preg_split("/[\s,]+/", $chapter->img, -1, PREG_SPLIT_NO_EMPTY);
-        if (empty($chapter->typeImg)){
-            for ($i=0;$i<count($array_img);$i++){
-                $array_img[$i]='//i1.heymanga.me/'.$array_img[$i];
+        if (empty($chapter->typeImg)) {
+            for ($i = 0; $i < count($array_img); $i++) {
+                $array_img[$i] = '//i1.heymanga.me/' . $array_img[$i];
             }
         }
         $json = request()->cookie('flmhistory');
@@ -55,6 +57,28 @@ class MangaController extends Controller
 
 
         return response()->view('chapter.chapter_show', compact('manga', 'chapter', 'array_img', 'next_link', 'prev_link', 'main_genre'))->cookie('flmhistory', $json, 5040);
+    }
+
+    public function create()
+    {
+
+        return view('manga.manga_create');
+    }
+
+    public function store(\Illuminate\Http\Request $request)
+    {
+//        dd($request->all());
+        $image = '';
+        foreach ($request->image as $item) {
+            $image .= $item . ',';
+        }
+        UserUpload::create([
+            'manga_name' => $request->manga_name,
+            'chap_name' => $request->chap_name,
+            'user_id' => auth()->user()->id,
+            'image' => $image
+        ]);
+        return back();
     }
 
 }
