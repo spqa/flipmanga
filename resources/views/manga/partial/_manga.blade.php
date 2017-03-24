7b24de40b5c72ecd98c70a78fbf11c6441e62fe7
super <qanhdk@gmail.com>
2017-03-24 16:59:01 +0700

    change index page

--- resources/views/manga/partial/_manga.blade.php
+++ resources/views/manga/partial/_manga.blade.php
@@ -24,10 +24,10 @@
                 <span class="badge green white-text past-timer">{{$item->updated_at->diffForHumans()}}</span>
             </div>
         </div>
-        <div class="card-reveal">
-            <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-            <p>Here is some more information about this product that is only revealed once clicked on.</p>
-        </div>
+        {{--<div class="card-reveal">--}}
+            {{--<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>--}}
+            {{--<p>Here is some more information about this product that is only revealed once clicked on.</p>--}}
+        {{--</div>--}}
     </div>
 </div>
 
