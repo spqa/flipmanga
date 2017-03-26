36d9ecf05be2237ed8f77753964303c1fa42ac26
super <qanhdk@gmail.com>
2017-03-26 19:09:19 +0700

    prevent cache

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -62,7 +62,7 @@
     <!-- Default Theme -->
     <link rel="stylesheet" href="{{asset('css/owl.theme.css')}}">
 
-    <link rel="stylesheet" href="{{asset('css/style.css?v=0.5')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css?v=0.6')}}">
 </head>
 
 <body>
@@ -282,7 +282,7 @@
 </script>
 <!-- Go to www.addthis.com/dashboard to customize your tools -->
 <script src="/js/mangacard.template.js"></script>
-<script src="{{asset('js/script.js?v=0.6')}}"></script>
+<script src="{{asset('js/script.js?v=0.8')}}"></script>
 <script type="text/javascript" src="http://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-57a557dc5a728f4b"></script>
 <script id="cid0020000150789223515" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js"
         style="width: 200px;height: 300px;">{
