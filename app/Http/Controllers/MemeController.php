b7b35787ec9c089ed6613876a3d2b0436487fd68
super <qanhdk@gmail.com>
2017-04-04 13:06:08 +0700

    meme editor

--- app/Http/Controllers/MemeController.php
+++ app/Http/Controllers/MemeController.php
@@ -30,9 +30,16 @@ class MemeController extends Controller
     }
     public function generateMeme($image,$up,$down){
         $img=\Intervention\Image\Facades\Image::make($image);
-        $font_path=public_path('fonts/impact.ttf');
+        $font_path=public_path('fonts/unicode.impact.ttf');
         $name=time();
-        $img->text($up, 200, 0, function ($font)use ($font_path) {
+        $img->text($up, $img->width()/2, 20, function ($font)use ($font_path) {
+            $font->file($font_path);
+            $font->size(34);
+            $font->color("#ffffff");
+            $font->align('center');
+            $font->valign('top');
+        });
+        $img->text($down, $img->width()/2, $img->height()-40, function ($font)use ($font_path) {
             $font->file($font_path);
             $font->size(34);
             $font->color("#ffffff");
--- resources/views/meme/create.blade.php
+++ resources/views/meme/create.blade.php
@@ -14,11 +14,12 @@
             <form class="col s12">
                 <div class="row">
                     <div class="input-field col s12 m6 l6">
-                        <input placeholder="tiêu đề ảnh" id="title" type="text" class="validate">
+                        <input placeholder="tiêu đề ảnh" id="title" type="text" class="validate" required>
                         <label for="title">Tiêu đề</label>
                     </div>
                     <div class="input-field col s12 m6 l6">
-                        <input id="image" type="url" class="validate" placeholder="link ảnh">
+                        <input value="http://i.memeful.com/template/521ee7a0bf0fb/LR8ZQwv.jpg" id="image" type="url"
+                               class="validate" placeholder="link ảnh" required>
                         <label for="image">Ảnh</label>
                     </div>
                 </div>
@@ -26,7 +27,9 @@
 
                 <div class="row">
                     <div class="col s12 m6 l6">
-                        <img class="responsive-img" id="preview-image" src="http://i.memeful.com/template/521ee7a0bf0fb/LR8ZQwv.jpg">
+                        <img class="responsive-img" id="preview-image"
+                             src="http://i.memeful.com/template/521ee7a0bf0fb/LR8ZQwv.jpg">
+                        <canvas id="editor"></canvas>
                     </div>
                     <div class="col s12 m6 l6">
                         <div class="input-field">
@@ -37,8 +40,11 @@
                             <input id="bottom-text" type="text">
                             <label for="bottom-text">text phía dưới</label>
                         </div>
-                        <button class="btn btn-preview col s6" type="submit"><i class="material-icons left">local_see</i>Xem trước</button>
-                        <button class="btn green col s6" type="submit"><i class="material-icons left">save</i>Đăng ảnh </button>
+                        <button class="btn btn-preview col s6" type="submit"><i
+                                    class="material-icons left">local_see</i>Xem trước
+                        </button>
+                        <button class="btn green col s6" type="submit"><i class="material-icons left">save</i>Đăng ảnh
+                        </button>
                     </div>
                 </div>
 
@@ -52,32 +58,52 @@
     </div>
 @endsection
 @section('page_script')
+    <script src="//cdn.jsdelivr.net/g/masonry@4.1.1,fabric.js@1.7.9(fabric.min.js)"></script>
     <script>
         $('#image').keyup(function (e) {
-            if($(this).val()){
-                $('#preview-image').attr('src',$(this).val());
+
+            if ($(this).val()) {
+                $('#preview-image').attr('src', $(this).val());
+                var canvas =new fabric.Canvas('editor');
+                var source=$(this).val();
+                fabric.Image.fromURL(source, function(oImg) {
+                    canvas.add(oImg);
+                });
+
             }
         });
 
-        $('#preview-image').on('error',function (e) {
-            $('#preview-image').attr('src','/404.png');
+        $('#preview-image').on('error', function (e) {
+            $('#preview-image').attr('src', '/404.png');
         });
-        $('.btn-preview').click(function (e) {
-            e.preventDefault();
-            $.ajax({
-                url:'/api/meme/preview',
-                method:'POST',
-                data:{
-                    image:$('#preview-image').attr('src'),
-                    up:$('#upper-text').val(),
-                    down:$('#bottom-text').val()
-                },
-                success:function (data) {
-                    $('#preview-image').attr('src','/storage/images/temp/'+data+'.jpg');
-                }
+        //        $('.btn-preview').click(function (e) {
+        //            e.preventDefault();
+        //            $.ajax({
+        //                url:'/api/meme/preview',
+        //                method:'POST',
+        //                data:{
+        //                    image:$('#image').val(),
+        //                    up:$('#upper-text').val(),
+        //                    down:$('#bottom-text').val()
+        //                },
+        //                success:function (data) {
+        //                    $('#preview-image').attr('src','/storage/images/temp/'+data+'.jpg');
+        //                }
+        //
+        //            });
+        //
+        //        });
+        //        var canvas = new fabric.Canvas('editor');
+        //        canvas.setWidth(500);
+        //        canvas.setHeight(500);
+        //        var imgElement = document.getElementById('preview-image');
+        //        var imgInstance = new fabric.Image(imgElement, {
+        //            left: 100,
+        //            top: 100,
+        //        });
+        //        canvas.add(imgInstance);
+        //        var text = new fabric.Text('hello world', { left: 100, top: 100 });
+        //        canvas.add(text);
 
-            });
-
-        });
     </script>
 @endsection
